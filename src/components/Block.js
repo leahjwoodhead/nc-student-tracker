0afc4b658c8dace9f1a770e37324875305104af1
Josh Peachey <joshpeachey24@gmail.com>
2020-12-03 16:42:33 +0000

    BlockBreakdown advance button removes student - working on adding to next block

---
+++ src/components/Block.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const BlockSection = styled.ul`
+  background-color: rgba(255, 255, 255, 0.8);
+  border: 1px solid rgba(0, 0, 0, 0.8);
+  padding: 20px;
+  margin: 10px;
+  font-size: 1.5rem;
+  text-align: center;
+`;
+const StudentCard = styled.li`
+  background-color: white;
+  list-style-type: none;
+  width: 80%;
+  margin: 1rem auto;
+  padding: 1rem;
+  box-shadow: 2px 2px 5px grey;
+`;
+
+const Block = (props) => {
+  console.log(props);
+  return (
+    <BlockSection>
+      <p>
+        Students in <strong>{props.block}</strong>: {props.students.length}
+      </p>
+      {props.students.map((student) => {
+        return (
+          <StudentCard key={student._id}>
+            <p>{student.name}</p>
+            <div
+              onClick={() => {
+                props.removeFromBlock(student._id);
+              }}
+            >
+              <button
+                id='progress-button'
+                onClick={() => {
+                  console.log('Clicked to advance');
+                  props.advanceStudent(student);
+                }}
+              >
+                Advance
+              </button>
+            </div>
+            <button
+              id='progress-button'
+              onClick={() => {
+                console.log('Clicked to resit');
+                props.resitStudent();
+              }}
+            >
+              Resit
+            </button>
+          </StudentCard>
+        );
+      })}
+    </BlockSection>
+  );
+};
+
+export default Block;
--- src/components/BlockBreakdown.js
+++ src/components/BlockBreakdown.js
@@ -1,42 +1,132 @@
 import React, { Component } from 'react';
 import { fetchStudents } from './api';
-import Block from './FundamentalsBlock';
+import Block from './Block';
 
 class BlockBreakdown extends Component {
-    state = {
-        students: [],
-        fundamentals: [],
-        backend: [],
-        frontend: [],
-        project: [],
-        isLoading: true
-    }
+  state = {
+    students: [],
+    fundamentals: [],
+    backend: [],
+    frontend: [],
+    project: [],
+    isLoading: true,
+  };
 
-    componentDidMount() {
-        fetchStudents(false).then((studentsData) => {
-            const students = studentsData;
-            const fundamentals = students.filter(student => student.currentBlock === "fun")
-            const backend = students.filter(student => student.currentBlock === "be")
-            const frontend= students.filter(student => student.currentBlock === "fe")
-            const project = students.filter(student => student.currentBlock === "proj")
-            this.setState({students, fundamentals, backend, frontend, project, isLoading: false})
-        })
-    }
+  componentDidMount() {
+    fetchStudents(false).then((studentsData) => {
+      const students = studentsData;
+      const fundamentals = students.filter(
+        (student) => student.currentBlock === 'fun'
+      );
+      const backend = students.filter(
+        (student) => student.currentBlock === 'be'
+      );
+      const frontend = students.filter(
+        (student) => student.currentBlock === 'fe'
+      );
+      const project = students.filter(
+        (student) => student.currentBlock === 'proj'
+      );
+      this.setState({
+        students,
+        fundamentals,
+        backend,
+        frontend,
+        project,
+        isLoading: false,
+      });
+    });
+  }
 
-    render() {
-        const { students, isLoading } = this.state
-        if (isLoading) { 
-            return <p>Loading...</p>
-        }
-        return (
-            <div className="outerBlock">
-                <Block students={this.state.fundamentals}/>
-                <Block students={this.state.backend}/>
-                <Block students={this.state.frontend}/>
-                <Block students={this.state.project}/>
-            </div>
-        );
+  // Removes student from current block
+  removeFromBlock = (studentId) => {
+    this.setState((currState) => {
+      const newState = {
+        fundamentals: currState.fundamentals.filter(
+          (person) => person._id !== studentId
+        ),
+        backend: currState.backend.filter((person) => person._id !== studentId),
+        frontend: currState.frontend.filter(
+          (person) => person._id !== studentId
+        ),
+        project: currState.project.filter((person) => person._id !== studentId),
+      };
+      return newState;
+    });
+  };
+
+  // Students can only advance to the block after
+  // ['Fundamentals','Backend','Frontend','Project']
+  // [1,2,3,4]
+  // takes student object as argument
+  // e.g. IF student.currentBlock === 'fun', this.state.backend.unshift(student)
+
+  advanceStudent = (student) => {
+    this.setState((currState) => {
+      const fundamentals = currState.fundamentals;
+      const backend = currState.backend;
+      const frontend = currState.frontend;
+      const project = currState.project;
+
+      if (student.currentBlock === 'fun') {
+        backend.unshift(student);
+      } else if (student.currentBlock === 'be') {
+        frontend.unshift(student);
+      } else if (student.currentBlock === 'fe') {
+        project.unshift(student);
+      } else if (student.currentBlock === 'proj') {
+        console.log('GRADUATED!');
+      }
+
+      const newState = {
+        backend: backend,
+        frontend: frontend,
+        project: project,
+      };
+      return newState;
+    });
+  };
+
+  resitStudent = () => {};
+
+  render() {
+    const { students, isLoading } = this.state;
+    if (isLoading) {
+      return <p>Loading...</p>;
     }
+    return (
+      <div className='outerBlock'>
+        <Block
+          block='Fundamentals'
+          students={this.state.fundamentals}
+          removeFromBlock={this.removeFromBlock}
+          advanceStudent={this.advanceStudent}
+          resitStudent={this.resitStudent}
+        />
+        <Block
+          block='Backend'
+          students={this.state.backend}
+          removeFromBlock={this.removeFromBlock}
+          advanceStudent={this.advanceStudent}
+          resitStudent={this.resitStudent}
+        />
+        <Block
+          block='Frontend '
+          students={this.state.frontend}
+          removeFromBlock={this.removeFromBlock}
+          advanceStudent={this.advanceStudent}
+          resitStudent={this.resitStudent}
+        />
+        <Block
+          block='Project'
+          students={this.state.project}
+          removeFromBlock={this.removeFromBlock}
+          advanceStudent={this.advanceStudent}
+          resitStudent={this.resitStudent}
+        />
+      </div>
+    );
+  }
 }
 
 export default BlockBreakdown;
